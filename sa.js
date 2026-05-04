a86a54b2e67ee2950e5d960cf3088d8abd3c1a85
umar <umarzokirov911@gmail.com>
2026-05-04 15:28:18 +0500

    as

---
+++ sa.js
@@ -0,0 +1,28 @@
+// let elem = document.querySelector("#form")
+// elem.hasAttribute("method") // true
+// elem.getAttribute("action") // "post"
+// elem.setAttribute("class", "second-form")
+// elem.removeAttribute("name") // "get"
+// console.log(elem.attributes);
+
+
+
+
+// let names = ["mike", "john", "susan"]
+// let ul = document.createElement("ul")
+// for(let name of names){
+//     let li = document.createElement("li")
+//     li.innerHTML = name
+//     ul.appendChild(li)
+// }
+
+// let namesDiv = document.querySelector(".names")
+// namesDiv.appendChild(ul)
+// console.log(ul);
+
+
+// console.log(namesDiv.parentElement);
+// console.log(namesDiv.children);
+
+
+let elem = document.querySelector("th")
